43d5b7fe1fe2935348a0772856f4ddf25b125d4c
Peter Hemsing <peterhemsing@gmail.com>
2021-06-06 22:05:42 +0200

    edit video loop

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,7 +4,7 @@ import video from "../video/birddogs_video.mp4";
 const Video = () => {
   return (
     <div>
-      <video autoPlay="autoplay" loop="loop" muted="muted" style={{objectFit: "cover", width: "100%", height: "100vh"}}>
+      <video autoPlay="autoplay" loop="loop" muted="muted" style={{objectFit: "cover", width: "100%", height: "100%"}}>
         <source src={video} type="video/mp4" />
       </video>
     </div>
