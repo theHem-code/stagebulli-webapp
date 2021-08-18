4211b5ae374959e3107f64347c52af898dcc91be
theHem-code <peterhemsing@gmail.com>
2021-08-18 11:20:18 +0200

    reset to previous commit

--- src/pages/Impressions.js
+++ src/pages/Impressions.js
@@ -8,10 +8,10 @@ import NavbarNew from "../components/Navbar/NavbarNew";
 import Footer from "../components/Footer";
 
 const Impressions = () => {
-
   useLayoutEffect(() => {
     window.scrollTo(0, 0);
   });
+//comment
 
   // useEffect(() => {
   //   fetch("https://api.cloudinary.com/v1_1/dxkj8owwl/image/upload")
@@ -20,17 +20,18 @@ const Impressions = () => {
   //       console.log(data);
   //     });
   // }, []);
+  
+  // commen
 
   return (
     <div>
-      <NavbarNew />
       <main style={{ height: "100vh" }}>
-        <div className="artists-container">
-          <div className="header">
-            <h1>Live - Impressionen</h1>
+        <div className="container artists-container">
+          <div className="row header">
+            <h1 className="col-12">Live - Impressionen</h1>
           </div>
         </div>
-        <div className="container">
+        <div className="image-grid">
           <CloudinaryContext cloudName="dxkj8owwl">
             <div>
               <Image
@@ -43,42 +44,8 @@ const Impressions = () => {
         </div>
       </main>
       <Footer />
-
     </div>
   );
 };
 
-
-
 export default Impressions;
-
-// {
-  /* <div className="gallery">
-  <CloudinaryContext cloudName="dxkj8owwl">
-    {gallery.map((data) => {
-      return (
-        <div className="responsive" key={data.public_id}>
-          <div className="img">
-            <a
-              target="_blank"
-              href={`http://res.cloudinary.com/christekh/image/upload/${data.public_id}.jpg`}
-            >
-              <Image publicId={data.public_id}>
-                <Transformation
-                  crop="scale"
-                  width="300"
-                  height="200"
-                  dpr="auto"
-                  responsive_placeholder="blank"
-                />
-              </Image>
-            </a>
-            <div className="desc">Created at {data.created_at}</div>
-          </div>
-        </div>
-      );
-    })}
-  </CloudinaryContext>
-  <div className="clearfix"></div>
-</div>; */
-// }
